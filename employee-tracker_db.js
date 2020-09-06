482d9934da7f89aa99463936a2dd789f86319cf9
Kmarzi <k.l.marziali@gmail.com>
2020-09-06 16:27:21 -0400

    added questions

--- employee-tracker_db.js
+++ employee-tracker_db.js
@@ -1,6 +1,12 @@
-var mysql = require("mysql");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-var connection = mysql.createConnection({
+// const generateMarkdown = require("./generateMarkdown");
+
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -27,3 +33,91 @@ function afterConnection() {
     connection.end();
   });
 }
+
+///QUESTIONS
+const questions = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    choices: [],
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Which employee manage",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "what comand should the user run to install the dependancies?",
+    default: "npm i",
+    name: "install"
+  },
+  {
+    type: "input",
+    message: "How does this user use your application?",
+    name: "usage"
+  },
+  {
+    type: "list",
+    message: "What kind of licensing does your application have?",
+    name: "license",
+    choices: ["MIT", "APACHE2.0", "GPL3.0", "BSD", "NONE"]
+  },
+  {
+    type: "input",
+    message: "Would you like your users to contribute to your application?",
+    name: "contributing"
+  },
+  {
+    type: "input",
+    message: "How do your users test your application?",
+    name: "tests"
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "githubUsername"
+  },
+  {
+    type: "input",
+    message: " What is your email address?",
+    name: "email"
+  }
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function for prompting questions
+function init() {
+  inquirer.prompt(questions).then(answers => {
+    console.log(JSON.stringify(answers, null, 2));
+
+    // 1. pass answers into generateMarkdown() and save result in a variable
+    const md = generateMarkdown(answers);
+    console.log(md);
+
+    fs.writeFile("README1.md", md, err => {
+
+    }
+
+      // 2. pass result from line above into writeFile()
+      //writeToFile("README.md", md);
+    )
+  });
+}
+
+// function call to initialize program
+init();
--- employee-tracker_db.sql
+++ employee-tracker_db.sql
@@ -2,15 +2,40 @@ DROP DATABASE IF EXISTS employee-tracker_db;
 CREATE DATABASE employee-tracker_db;
 USE employee-tracker_db;
 
-CREATE TABLE songs (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(45) NULL,
-  artist VARCHAR (45) NULL,
-  genre VARCHAR (45)
+  department_name VARCHAR(30) NULL,
   PRIMARY KEY(id)
 );
 
-INSERT INTO songs(title, artist, genre)
-VALUES ("reflections", "misterwives", "pop"), ("vagabond", "misterwives", "pop"), ("oceans","misterwives", "pop")
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR (30) NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
+  PRIMARY KEY(id)
+);
+
+INSERT INTO department(id, department_name)
+VALUES ("Dental")
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Dental Hygienist", 70000, 89)
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Kim", "Marziali", 1)
 
 
+SELECT department,title, salary
+FROM department
+INNER JOIN role
+ON department.department.id = role.department_ID
